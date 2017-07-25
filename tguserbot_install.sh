33c86c315ad240d0cf3c29829e3b35256f972758
peppelg <peppelg@bmtrad.it>
2017-07-25 16:33:25 +0200

    pedo scitstorm

--- tguserbot_install.sh
+++ tguserbot_install.sh
@@ -1,4 +1,4 @@
-apt-get -y install php php-mbstring php-xml php-gmp php-curl php-libsodium php-mcrypt php-bcmath
+apt-get -y install php php-mbstring php-xml php-gmp php-curl php-mcrypt php-bcmath
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/bin --filename=composer
 git clone https://github.com/peppelg/TGUserbot
 cd TGUserbot
