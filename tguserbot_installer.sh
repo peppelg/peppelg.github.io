cdedccf62beafc934095b45ab9daebbfd38b9da1
Giuseppe <peppelg@bmtrad.it>
2017-12-31 13:16:09 +0100

    V2

--- tguserbot_installer.sh
+++ tguserbot_installer.sh
@@ -1,6 +1,8 @@
-apt-get -y install php php-mbstring php-xml php-gmp php-curl php-mcrypt php-bcmath
+sudo -i
+apt-get -y install git zip php php-mbstring php-xml php-gmp php-curl php-mcrypt php-bcmath php-zip php-json php-sockets
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/bin --filename=composer
 git clone https://github.com/peppelg/TGUserbot
 cd TGUserbot
 composer update
 echo OK
+exit
