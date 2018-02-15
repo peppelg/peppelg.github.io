b478db9f9926ef4ea6388ab69dd8afed48fe438a
Giuseppe <peppelg@bmtrad.it>
2018-02-15 17:37:34 +0100

    tecnologia

--- tguserbot_installer.sh
+++ tguserbot_installer.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run this shit as root"
+  exit
+fi 
 sudo apt-get -y install git zip screen php php-mbstring php-xml php-gmp php-curl php-bcmath php-zip php-json python
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/bin --filename=composer
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
 git clone https://github.com/peppelg/TGUserbot
 cd TGUserbot
 composer update
