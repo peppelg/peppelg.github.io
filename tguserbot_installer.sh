df5b84bf76cda377a178bf50eea60b8fd8924cc6
Giuseppe <peppelg@bmtrad.it>
2018-02-28 14:48:47 +0100

    php-cli

--- tguserbot_installer.sh
+++ tguserbot_installer.sh
@@ -3,12 +3,13 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run this shit as root"
   exit
 fi 
-sudo apt-get -y install git zip screen php php-mbstring php-xml php-gmp php-curl php-bcmath php-zip php-json python
+sudo apt-get -y install git zip screen curl python php php-mbstring php-xml php-gmp php-curl php-bcmath php-zip php-json
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
 git clone https://github.com/peppelg/TGUserbot
 cd TGUserbot
 composer update
 rm LICENSE
 rm README.md
+sudo apt-get -y install php-cli
 echo OK
 exit
