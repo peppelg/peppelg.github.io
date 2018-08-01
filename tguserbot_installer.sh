f2656091cb343bf2bc171d8b413e4257f7bc9970
Giuseppe <me@peppelg.space>
2018-08-01 18:58:25 +0200

    V4

--- tguserbot_installer.sh
+++ tguserbot_installer.sh
@@ -9,12 +9,11 @@ apt-get install -y language-pack-en-base
 add-apt-repository -y ppa:ondrej/php
 apt-get update
 apt-get -y install git zip screen curl python php7.2 php7.2-mbstring php7.2-xml php7.2-gmp php7.2-curl php7.2-bcmath php7.2-zip php7.2-json php7.2-cli
-curl -sS https://getcomposer.org/installer | php7.2 -- --install-dir=/usr/bin --filename=composer
-git clone https://github.com/peppelg/TGUserbot
+mkdir TGUserbot
 cd TGUserbot
-composer update
-rm LICENSE
-rm README.md
+wget https://github.com/peppelg/TGUserbot/raw/master/TGUserbot.phar
+wget https://raw.githubusercontent.com/peppelg/TGUserbot/master/functions.php
+wget https://raw.githubusercontent.com/peppelg/TGUserbot/master/bot.php
 apt-get -y install php7.2-cli
 update-alternatives --set php /usr/bin/php7.2
 echo OK
